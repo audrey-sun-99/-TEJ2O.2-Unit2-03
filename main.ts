b2b19efbfd86f94340cad0f260f8857f9e498fac
audrey-sun-99 <audrey.sun@stu.ocsb.ca>
2026-02-17 13:13:44 -0500

    this should do basic math

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Audrey
+ * Created on: feb 2026
+ * This program should do basic math
 */
+basic.pause(1000)
+basic.clearScreen()
 
-basic.showString('Hello, World!')
+basic.showString("A rectangle has dimensions 5 cm & 3 cm.The perimeter would be:")
+basic.showString((2*5+3). toString())
+basic.showString("The area would be")
+basic.showString((5*3). toString())
